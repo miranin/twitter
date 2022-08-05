4353589570b2efff051e4beae00215c392d05ecc
Tamirlan Aubakirov <tamirlan.aubakirov@alumni.nu.edu.kz>
2022-08-05 10:06:34 +0600

    Initial Commit

---
+++ Twitter/TwitterApp.swift
@@ -0,0 +1,17 @@
+//
+//  TwitterApp.swift
+//  Twitter
+//
+//  Created by Tamirlan Aubakirov on 05.08.2022.
+//
+
+import SwiftUI
+
+@main
+struct TwitterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
